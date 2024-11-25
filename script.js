8849302fd3088b159b92e2de9a3349347b9bf3ee
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2024-11-25 11:09:47 +0000

    feat: add style.css file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector("#grid-container")
 
 
 function createGrid() {
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < 25; i++) {
         const gridSqaure = document.createElement("div")
         gridSqaure.id = "grid-square";
         container.append(gridSqaure);
